2b45068b942f5fc960ed7f645ba5861af967a6aa
laonanhai <1422941961@qq.com>
2023-11-22 17:21:58 +0800

    前端 注册界面参数校验

--- project-frontend/src/components/welcome/RegisterPage.vue
+++ project-frontend/src/components/welcome/RegisterPage.vue
@@ -1,40 +1,52 @@
 <template>
     <div style="text-align: center;margin: 0 20px">
-        <div style="margin-top: 120px">
+        <div style="margin-top: 100px">
             <div style="font-size: 25px;font-weight: bold">注册</div>
             <div style="font-size: 14px;color: gray;margin-top: 10px">填写内容来完成注册</div>
         </div>
-        <div style="margin-top: 40px">
-            <el-input type="text" placeholder="用户名" v-model="form.username">
-                <template #prefix>
-                    <el-icon>
-                        <User/>
-                    </el-icon>
-                </template>
-            </el-input>
-            <el-input type="password" placeholder="密码" style="margin-top: 10px" v-model="form.password">
-                <template #prefix>
-                    <el-icon>
-                        <Lock/>
-                    </el-icon>
-                </template>
-            </el-input>
-            <el-input type="password" placeholder="确认密码" style="margin-top: 10px" v-model="form.passwordRepeat">
-                <template #prefix>
-                    <el-icon>
-                        <Lock/>
-                    </el-icon>
-                </template>
-            </el-input>
-            <el-input type="text" placeholder="邮箱" style="margin-top: 10px" v-model="form.email">
-                <template #prefix>
-                    <el-icon>
-                        <Message/>
-                    </el-icon>
-                </template>
-            </el-input>
-            <div style="margin-top: 12px">
-                <el-row>
+        <el-form style="margin-top: 40px"
+                 :model="form"
+                 :rules="rules"
+                @validate="myValidate"
+            >
+            <el-form-item prop="username">
+                <el-input type="text" placeholder="用户名" v-model="form.username">
+                    <template #prefix>
+                        <el-icon>
+                            <User/>
+                        </el-icon>
+                    </template>
+                </el-input>
+            </el-form-item>
+            <el-form-item prop="password">
+                <el-input type="password" placeholder="密码" v-model="form.password">
+                    <template #prefix>
+                        <el-icon>
+                            <Lock/>
+                        </el-icon>
+                    </template>
+                </el-input>
+            </el-form-item>
+            <el-form-item prop="passwordRepeat">
+                <el-input type="password" placeholder="确认密码" v-model="form.passwordRepeat">
+                    <template #prefix>
+                        <el-icon>
+                            <Lock/>
+                        </el-icon>
+                    </template>
+                </el-input>
+            </el-form-item>
+            <el-form-item prop="email">
+                <el-input type="text" placeholder="邮箱" v-model="form.email">
+                    <template #prefix>
+                        <el-icon>
+                            <Message/>
+                        </el-icon>
+                    </template>
+                </el-input>
+            </el-form-item>
+            <el-form-item prop="code">
+                <el-row style="width: 100%;">
                     <el-col :span="16">
                         <el-input type="text" placeholder="邮箱验证码" v-model="form.code">
                             <template #prefix>
@@ -45,25 +57,25 @@
                         </el-input>
                     </el-col>
                     <el-col :span="8" style="text-align: right">
-                        <el-button type="success">获取验证码</el-button>
+                        <el-button type="success" :disabled="!isEmail">获取验证码</el-button>
                     </el-col>
                 </el-row>
-            </div>
+            </el-form-item>
 
-
-            <div style="margin-top: 60px">
-                <el-button style="width: 200px;font-size: 16px" type="warning" @click="router.push('/register')">
-                    立即注册
-                </el-button>
-            </div>
-            <el-divider style="margin-top: 30px">
-                <span style="color: gray">已有账号?</span>
-            </el-divider>
-            <div style="margin-top: 10px">
-                <el-button  style="width: 200px;font-size: 16px" type="primary" @click="router.push('/')">立即登录
-                </el-button>
-            </div>
+        </el-form>
+        <div style="margin-top: 60px">
+            <el-button style="width: 200px;font-size: 16px" type="warning" @click="">
+                立即注册
+            </el-button>
         </div>
+        <el-divider style="margin-top: 30px">
+            <span style="color: gray">已有账号?</span>
+        </el-divider>
+        <div style="margin-top: 10px">
+            <el-button style="width: 200px;font-size: 16px" type="primary" @click="router.push('/')">立即登录
+            </el-button>
+        </div>
+
 
     </div>
 </template>
@@ -71,7 +83,7 @@
 <script setup>
     import {Check, Lock, Message, User} from "@element-plus/icons-vue";
     import router from "@/router";
-    import {ref} from "vue";
+    import {ref, reactive} from "vue";
 
     const form = ref({
         username: '',
@@ -80,7 +92,57 @@
         email: '',
         code: ''
     })
+    const validateUsername = (rule, value, callback) => {
+        if (value === '') {
+            callback(new Error('用户名不能为空'))
+        } else if (value.length > 20 || value.length < 3) {
+            callback(new Error('用户名长度为3~20位'))
+        } else if (!/^[\u4e00-\u9fa5_a-zA-Z0-9]+$/.test(value)) {
+            callback(new Error('用户名不能包含特殊字符'))
+        } else {
+            callback()
+        }
+    }
 
+    const validatePassword = (rule, value, callback) => {
+        if (value === '') {
+            callback(new Error('密码不能为空'))
+        } else if (value.length > 20 || value.length < 6) {
+            callback(new Error('密码长度为6~20位'))
+        } else {
+            callback()
+        }
+    }
+
+    const validatePasswordRepeat = (rule, value, callback) => {
+        if (value !== form.value.password) {
+            callback(new Error('密码不一致'))
+        } else {
+            callback()
+        }
+    }
+    const rules = reactive({
+        username: [{validator: validateUsername, trigger: ['blur', 'change']}],
+        password: [{validator: validatePassword, trigger: ['blur', 'change']}],
+        passwordRepeat: [
+            {validator: validatePasswordRepeat, trigger: ['blur', 'change']}
+        ],
+        email:[
+            {required: true, message: '请填写邮箱'},
+            {type: 'email', message: '请填写正确邮箱格式'}
+        ],
+        code: [
+            {required: true, message: '请填写验证码'},
+            {type: 'number', message: '验证码必须为数字'}
+        ]
+    })
+
+    const isEmail = ref(false)
+    const myValidate = (prop,isValid)=>{
+        if (prop === 'email'){
+            isEmail.value = isValid
+        }
+    }
 </script>
 
 <style scoped>
--- project-frontend/src/views/WelcomeView.vue
+++ project-frontend/src/views/WelcomeView.vue
@@ -5,9 +5,11 @@
                       src="https://ts1.cn.mm.bing.net/th/id/R-C.6b5df1bfe0e4778a44dba0753cd169c8?rik=QRQIMqvjWRCO5Q&riu=http%3a%2f%2fpic39.nipic.com%2f20140321%2f8857347_232251363165_2.jpg&ehk=7oAaMo6LCHJc%2bqpQ0IPvcH7v69jGRQhb2vDz%2fOd5720%3d&risl=&pid=ImgRaw&r=0"/>
         </div>
         <div style="width: 400px;background-color: white">
-            <transition name="el-fade-in-linear">
-                <router-view/>
-            </transition>
+            <router-view v-slot="{ Component }">
+                <transition name="el-fade-in-linear">
+                    <component :is="Component"/>
+                </transition>
+            </router-view>
         </div>
     </div>
 </template>
